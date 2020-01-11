1081b126cae365b7543e8f0158eb5b92ad223f3e
Sergey <sergey.mindblast@mail.ru>
2020-01-11 03:06:40 +0300

    add core components

---
+++ public/index.php
@@ -0,0 +1,42 @@
+<?php
+
+use App\Controllers\OrderController;
+use Symfony\Component\EventDispatcher\EventDispatcher;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
+use Symfony\Component\HttpKernel\Controller\ControllerResolver;
+use Symfony\Component\HttpKernel\EventListener\RouterListener;
+use Symfony\Component\HttpKernel\HttpKernel;
+use Symfony\Component\Routing\Matcher\UrlMatcher;
+use Symfony\Component\Routing\RequestContext;
+use Symfony\Component\Routing\Route;
+use Symfony\Component\Routing\RouteCollection;
+
+require __DIR__.'/../vendor/autoload.php';
+
+try {
+    $routes = new RouteCollection();
+    $routes->add('order_create', new Route('/order', [
+            '_controller' => [OrderController::class, 'index']]
+    ));
+
+    $request = Request::createFromGlobals();
+
+    $matcher = new UrlMatcher($routes, new RequestContext());
+
+    $dispatcher = new EventDispatcher();
+    $dispatcher->addSubscriber(new RouterListener($matcher, new RequestStack()));
+
+    $controllerResolver = new ControllerResolver();
+    $argumentResolver = new ArgumentResolver();
+
+    $kernel = new HttpKernel($dispatcher, $controllerResolver, new RequestStack(), $argumentResolver);
+
+    $response = $kernel->handle($request);
+    $response->send();
+
+    $kernel->terminate($request, $response);
+}catch (Exception $exception){
+    var_dump($exception);die();
+}
