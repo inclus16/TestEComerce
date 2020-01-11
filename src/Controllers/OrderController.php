51b6110e6261e399e84e4141a517602d2c469822
Sergey <sergey.mindblast@mail.ru>
2020-01-11 03:07:05 +0300

    forget to commit out of public fir

--- src/Controllers/OrderController.php
+++ src/Controllers/OrderController.php
@@ -4,7 +4,13 @@
 namespace App\Controllers;
 
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
 class OrderController
 {
-
+    public function index(Request $request)
+    {
+        return new Response('awewae',200);
+    }
 }
